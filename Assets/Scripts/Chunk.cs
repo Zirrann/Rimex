f720231497868b8b4e3c1d9e07ee058b32a994f4
Zirrann <140590309+Zirrann@users.noreply.github.com>
2024-09-06 10:11:12 +0200

    Simple biomes generation

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -9,17 +9,27 @@ public class Chunk
     private GameObject chunkParent = GameObject.Find("Chunks");
     public GameObject chunkObject;
 
-    public Chunk(Vector2Int chunkCoord)
-    {
-        this.position = chunkCoord;
-        terrain = TerrainGenerator.GenerateChunk(position);
 
-        chunkObject = new GameObject($"Chunk_{chunkCoord.x}_{chunkCoord.y}");
+    public Chunk(Vector2Int chunkCoord, Material material, Mesh terrain) 
+    { 
+        this.position = chunkCoord;
+        this.terrain = terrain;
+
+        Initialize(material);
+    }
+
+    private void Initialize(Material material) {
+        chunkObject = new GameObject($"Chunk_{position.x}_{position.y}");
         chunkObject.tag = "Chunk";
-        chunkObject.transform.position = new Vector3(chunkCoord.x * chunkSize - worldSize / 2, 0, chunkCoord.y * chunkSize - worldSize / 2);
+        // chunkObject.transform.position = new Vector3(position.x * chunkSize - worldSize / 2, 0, position.y * chunkSize - worldSize / 2);
+        chunkObject.transform.position = new Vector3(position.x * chunkSize , 0, position.y * chunkSize);
+        if (material == null)
+        {
+            material = new Material(Shader.Find("Standard"));
+        }
 
         chunkObject.AddComponent<MeshFilter>().mesh = terrain;
-        chunkObject.AddComponent<MeshRenderer>().material = new Material(Shader.Find("Standard"));
+        chunkObject.AddComponent<MeshRenderer>().material = material;
         chunkObject.AddComponent<MeshCollider>();
         chunkObject.transform.parent = chunkParent.transform;
     }
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -3,21 +3,23 @@ using static Properties;
 
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class TerrainGenerator : MonoBehaviour
 {
     public Dictionary<Vector2Int, Chunk> terrainChunks = new Dictionary<Vector2Int, Chunk>();
 
+    public Material grassMaterial;
+
 
     void Start()
     {
-       GenerateTerrain();
+        GenerateTerrain();
     }
 
     void GenerateTerrain()
     {
-        Debug.Log(chunkSize);
         int numChunks = worldSize / chunkSize;
 
         for (int x = 0; x < numChunks; x++)
@@ -26,8 +28,15 @@ public class TerrainGenerator : MonoBehaviour
             {
                 Vector2Int chunkCoord = new Vector2Int(x, z);
                 Mesh chunkMesh = GenerateChunk(chunkCoord);
-                terrainChunks.Add(chunkCoord, new Chunk(chunkCoord));
+                chunkMesh.RecalculateNormals();
+                terrainChunks.Add(chunkCoord, new Chunk(chunkCoord, grassMaterial, chunkMesh));
             }
+        }
+
+        var keys = terrainChunks.Keys.ToList();
+        foreach (var key in keys)
+        {
+            terrainChunks[key] = AdjustChunkToNeighbours(terrainChunks[key]);
         }
     }
 
@@ -37,12 +46,15 @@ public class TerrainGenerator : MonoBehaviour
         int[] triangles = new int[chunkSize * chunkSize * 6];
         Vector2[] uvs = new Vector2[vertices.Length];
 
+        (float firstOctaceValue, int octavesCount, float frequencyScale, float heightScale, float exp) = GetBiomeParams(chunkCoord.x, chunkCoord.y);
+
         for (int i = 0, z = 0; z <= chunkSize; z++)
         {
             for (int x = 0; x <= chunkSize; x++, i++)
             {
 
-                float y = GetNoiceValue(x + chunkCoord.x * chunkSize, z + chunkCoord.y * chunkSize);
+                float y = GetNoiceValue(x + chunkCoord.x * chunkSize, z + chunkCoord.y * chunkSize,
+                    firstOctaceValue, octavesCount, frequencyScale, heightScale, exp);           
                 vertices[i] = new Vector3(x, y, z);
                 uvs[i] = new Vector2((float)x / chunkSize, (float)z / chunkSize);
             }
@@ -71,13 +83,12 @@ public class TerrainGenerator : MonoBehaviour
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.uv = uvs;
-        mesh.RecalculateNormals();
 
         return mesh;
     }
 
 
-    private static float GetNoiceValue(float x, float y) {
+    private static float GetNoiceValue(float x, float y, float firstOctaceValue, int octavesCount, float frequencyScale, float heightScale, float exp ) {
 
         x = x / worldSize;
         y = y / worldSize;
@@ -86,16 +97,137 @@ public class TerrainGenerator : MonoBehaviour
         float ocatave = firstOctaceValue;
         float n = 0;
 
-        for (int i = 0; i < octaves; i++) 
+        for (int i = 0; i < octavesCount; i++)
         {
-            noice += Mathf.PerlinNoise(ocatave * x * frequencyScale, ocatave * y * frequencyScale) * heightScale / ocatave;
+            noice += Mathf.PerlinNoise(x * frequencyScale * ocatave, y * frequencyScale * ocatave) * heightScale / ocatave;
             n += 1 / ocatave;
             ocatave *= 2;
-            
+
         }
 
         noice = noice / n;
+        noice = (float)Math.Pow(noice, exp) - heightScale / 2;
 
-        return (float)Math.Pow(noice, exp) - heightScale/2;
+        return noice;
     }
-}  
+
+
+    private static float GetBiome(float x, float y, int seed)
+    {
+        float biomeFrequency = 1.5f;
+       // x = (x + (seed & worldSize) / worldSize); 
+       // y = (y + (seed & worldSize) / worldSize);   
+        x = x / worldSize * chunkSize;
+        y = y / worldSize * chunkSize;
+
+        float  b = Mathf.PerlinNoise((x) * biomeFrequency, (y) * biomeFrequency);
+        
+        return b;
+    }
+
+    private static (float, int, float, float, float) GetBiomeParams(float x, float y) 
+    {
+
+        float biomeValue = GetBiome(x, y, 47231);
+
+
+        float firstOctaceValue;
+        float frequencyScale;
+        float heightScale;
+        float exp;
+        int octavesCount;
+
+
+
+        if (biomeValue < 0.3f)
+        {
+            heightScale = 10f;
+            frequencyScale = 2f;
+            firstOctaceValue = 1f;
+            octavesCount = 1;
+            exp = 0.6f;
+        }
+        else if (biomeValue < 0.4f)
+        {
+            heightScale = 12f;
+            frequencyScale = 2f;
+            firstOctaceValue = 1f;
+            octavesCount = 1;
+            exp = 0.6f;
+        }
+        else if (biomeValue < 0.5f)
+        {
+            heightScale = 16f;
+            frequencyScale = 2f;
+            firstOctaceValue = 2.5f;
+            octavesCount = 2;
+            exp = 0.7f;
+        }
+        else if (biomeValue < 0.6f)
+        {
+            heightScale = 30f;
+            frequencyScale = 2f;
+            firstOctaceValue = 3f;
+            octavesCount = 3;
+            exp = 0.8f;
+        }
+        else if (biomeValue < 0.7f)
+        {
+            heightScale = 45f;
+            frequencyScale = 4f;
+            firstOctaceValue = 2f;
+            octavesCount = 4;
+            exp = 1f;
+        }
+        else
+        {
+            heightScale = 60f;
+            frequencyScale = 4f;
+            firstOctaceValue = 2f;
+            octavesCount = 5;
+            exp = 1.1f;
+        }
+
+        return (firstOctaceValue, octavesCount, frequencyScale, heightScale, exp);
+    }
+
+
+    Chunk AdjustChunkToNeighbours(Chunk chunk)
+    {
+        Vector3[] vertices = chunk.terrain.vertices;
+        Vector2Int chunkCoord = chunk.position;
+
+        if (terrainChunks.TryGetValue(new Vector2Int(chunkCoord.x + 1, chunkCoord.y), out Chunk rightChunk))
+        {
+            Vector3[] neighborVertices = rightChunk.terrain.vertices;
+            for (int i = 0; i < chunkSize; i++)
+            {
+                int j = i * (chunkSize + 1) + chunkSize;
+                vertices[j] = new Vector3(vertices[j].x, neighborVertices[i * (chunkSize + 1)].y, vertices[j].z);
+            }
+        }
+
+        if (terrainChunks.TryGetValue(new Vector2Int(chunkCoord.x, chunkCoord.y + 1), out Chunk aboveChunk))
+        {
+            Vector3[] neighborVertices = aboveChunk.terrain.vertices;
+
+            for (int i = 0; i <= chunkSize; i++)
+            {
+                int j = chunkSize * (chunkSize + 1) + i;
+                vertices[j] = new Vector3(vertices[j].x, neighborVertices[i].y, vertices[j].z);
+            }
+        }
+
+        if (terrainChunks.TryGetValue(new Vector2Int(chunkCoord.x + 1, chunkCoord.y + 1), out Chunk cornerChunk))
+        {
+            Vector3[] neighborVertices = cornerChunk.terrain.vertices;
+            int j = vertices.Length - 1;
+            vertices[j] = new Vector3(vertices[j].x, neighborVertices[0].y, vertices[j].z);
+        }
+
+
+        chunk.terrain.vertices = vertices;
+       
+        return chunk;
+    }    
+}
