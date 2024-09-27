871aea53c5d98d189e0269396c49f76a4c39866e
Zirrann <140590309+Zirrann@users.noreply.github.com>
2024-09-27 13:03:03 +0200

    Folders refactorization

---
+++ Assets/Scripts/InventoryItedmData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(menuName = "Inventory Item Data")]
+    public class InventoryItedmData : ScriptableObject
+    {
+        public string id;
+        public string displayName;
+        public Sprite icon;
+        public GameObject prefab;
+    }
+}
